a894e81ed4367cd84f7d5a1fed9bb829f7188275
Juwami <sean.nguyenf@yahoo.com>
2019-05-23 00:05:24 -0400

    create renderBtn to add buttons to id btnArea

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,39 @@
+// DOM elements
+$addBtn = $('#addBtn')
+$gifArea = $('#gifArea')
+$gifText = $('#gifText')
+$btnArea = $('#btnArea')
 
+// JS elements
 let apiKey = '7NfyKqoRW6mHhbsfWjQPkIeS1zjsYiYW'
-let giphyCollectorArr = []
+let topics = []
+let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + 'cat' + '&api_key=' + apiKey + '&limit=10'
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-    
+$addBtn.on('click', function () {
+    topics.push($gifText.val())
+    console.log(topics)
+    renderBtn()
 })
+
+function renderBtn() {
+    $btnArea.empty()
+    for (i = 0; i < topics.length; i++) {
+        let a = $('<button>')
+        a.addClass('m-1 btn btn-dark')
+        a.text(topics[i])
+        a.attr('gif-name', topics[i])
+        $btnArea.append(a)
+    }
+}
+
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+// }).then(function(response) {
+//     let data = response.data
+//     console.log(response.data.length)
+//     console.log(response.data)
+//     for (i=0; i<response.data.lenth; i++) {
+
+//     }
+// })

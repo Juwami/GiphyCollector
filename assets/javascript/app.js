c0fc58f9e903db27b017fe25f8f89ede26381cc0
Juwami <sean.nguyenf@yahoo.com>
2019-05-25 01:47:41 -0400

    add class to pull attr gif-name

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $btnArea = $('#btnArea')
 // JS elements
 const apiKey = '7NfyKqoRW6mHhbsfWjQPkIeS1zjsYiYW'
 let topics = []
-let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + 'cat' + '&api_key=' + apiKey + '&limit=10'
+let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=' + apiKey + '&limit=10'
 
 // prevents user from spamming blank input, also pushes all inputs to array as upper case
 $addBtn.on('click', function () {
@@ -27,21 +27,29 @@ function renderBtn() {
     $btnArea.empty()
     for (i = 0; i < topics.length; i++) {
         let a = $('<button>')
-        a.addClass('m-1 btn btn-dark')
+        a.addClass('m-1 btn btn-dark gifNewBtn')
         a.text(topics[i])
         a.attr('gif-name', topics[i])
+        a.attr('id', topics[i])
         $btnArea.append(a)
     }
 }
 
-// $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-// }).then(function(response) {
-//     let data = response.data
-//     console.log(response.data.length)
-//     console.log(response.data)
-//     for (i=0; i<response.data.lenth; i++) {
+$(document).on('click', '.gifNewBtn', displayGifStats)
 
-//     }
-// })
+function displayGifStats() {
+    let gifName = $(this).attr('gif-name')
+    console.log(gifName)
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function(response) {
+    let data = response.data
+    console.log(response.data.length)
+    console.log(response.data)
+    for (i=0; i<response.data.lenth; i++) {
+
+    }
+})
+}

b99a6d91e1cfb6640b3d441d2e754317adbd5182
Juwami <sean.nguyenf@yahoo.com>
2019-05-28 16:34:25 -0400

    update ajax function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,17 +7,15 @@ $btnArea = $('#btnArea')
 // JS elements
 const apiKey = '7NfyKqoRW6mHhbsfWjQPkIeS1zjsYiYW'
 let topics = []
-let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=' + apiKey + '&limit=10'
 
 // prevents user from spamming blank input, also pushes all inputs to array as upper case
 $addBtn.on('click', function () {
     if ($gifText.val() === '') {
         return false
-    }
-    else {
-    topics.push($gifText.val().toUpperCase())
-    console.log(topics)
-    renderBtn()
+    } else {
+        topics.push($gifText.val().toUpperCase())
+        console.log(topics)
+        renderBtn()
     }
     $gifText.val('')
 })
@@ -40,16 +38,25 @@ $(document).on('click', '.gifNewBtn', displayGifStats)
 function displayGifStats() {
     let gifName = $(this).attr('gif-name')
     console.log(gifName)
+    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=' + apiKey + '&limit=10'
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-    let data = response.data
-    console.log(response.data.length)
-    console.log(response.data)
-    for (i=0; i<response.data.lenth; i++) {
+    // cleans area for new gifs
+    $gifArea.empty()
 
-    }
-})
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        let data = response.data
+        console.log(response.data.length)
+        console.log(response.data)
+        for (i = 0; i < response.data.lenth; i++) {
+            let gifDiv = $('<div>')
+            let rating = data.topics[i].rating
+            let p = $('<p>').text('Rating: ' + rating)
+            let gifImage = $('<image>')
+            animalImage
+            
+        }
+    })
 }
